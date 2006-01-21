642e2c882c8d841ba36f05614e9d6861c687d6a9
judas_iscariote <judas_iscariote@d7ba68e2-4efe-0310-949a-a4d2d18e640a>
2006-01-21 19:37:35 +0000

    some progress , broken at the moment

--- trunk/simplesvn.py
+++ trunk/simplesvn.py
@@ -1,24 +1,29 @@
 #! /usr/bin/env python
+# -*- coding: utf-8 -*-
 
-## Subversion Repository creation tool 
-##for SuSE Linux >=9.3  using mod_dav_svn and mod_authz_svn
-##Author :Cristian Rodriguez R.
-##License : BSD License 
+## Subversion Repository creation tool
+## for SUSE Linux 10.0 or later  using mod_dav_svn , mod_authz_svn and mod_auth_digest
+## Author :Cristian Rodriguez R.
+## License : BSD License
 
 from optparse import OptionParser
 from pwd import getpwnam
 from grp import getgrnam
 from os import chmod , sep , walk , lchown
-from os.path import exists , join 
+from os.path import exists , join
 from subprocess import call
-#from svn import core, repos , fs # WITFM ???
+import md5
+from svn import core, repos , fs
+from getpass import getpass
+from string import Template
 
 cli_parser = OptionParser(usage = "usage: %prog [-n repo] [ -t type ] [-u username]")
 
 cli_parser.set_defaults(filesystem="fsfs", location="/srv/svn/repos/" , configdir="/etc/apache2/conf.d/", authdir="/srv/svn/user_access/", apache_user="wwwrun" , apache_group="www")
 
 cli_parser.add_option("-n", "--name" , type="string", dest="repo" ,help="create repository REPO")
-cli_parser.add_option("-f", "--fs-type" , type="string",dest="filesystem" ,help="Filesystem (default: fsfs)")
+cli_parser.add_option("-f", "--fs-type" , type="string",dest="filesystem"
+,help="Filesystem (default and recommended: fsfs)")
 cli_parser.add_option("-l", "--location" , type="string" , dest="location" , help="Repository location (default: /srv/svn/repos/)")
 cli_parser.add_option("-d", "--configdir", type="string" , dest="configdir", help="Apache configuration files location (default: /etc/apache2/conf.d/ )")
 cli_parser.add_option("-a" , "--authdir" , type="string" , dest="authdir" , help="Directory where auth information is stored (default: /srv/svn/user_access/)")
@@ -29,80 +34,93 @@ cli_parser.add_option("-u","--username",type="string",dest="adminuser", help="Th
 (options, args) = cli_parser.parse_args()
 
 apacheuid = getpwnam(options.apache_user)[2]
-apachegid = getgrnam(options.apache_group)[2] 
+apachegid = getgrnam(options.apache_group)[2]
 
 # if user don't provide the repository name
-if options.repo is None:
-     cli_parser.error("You MUST tell me the repository name")
+if (options.repo is None):
+    cli_parser.error("You MUST tell me the repository name")
 elif exists(options.location + options.repo):
-	cli_parser.error("Repository already exists")
-if options.adminuser is None:
+    cli_parser.error("Repository already exists")
+if (options.adminuser is None):
     cli_parser.error("You MUST tell me what username you want")
 try :
-        template = open( options.configdir + 'template.subversion').read()
-        withrepo = template.replace('{-repository-}', options.repo)
-        withauthdir = withrepo.replace('{-authdir-}', options.authdir )
-        witheverything = withauthdir.replace('{-location-}',options.location)
+    template = Template(file( options.configdir + 'template.subversion').read())
+    parsed = template.safe_substitute(repository=options.repo, authdir=options.authdir,location=options.location )
+
 except IOError :
-    cli_parser.error( "Repository template do not exist (RTFM)")
+    cli_parser.error( "Repository template do not exist")
 else :
     try:
-        config_file = open(options.configdir + options.repo + '.svnrepo' ,'w')
-        config_file.write(witheverything)
+        config_file = file(options.configdir + options.repo + '.conf' ,'wb')
+        config_file.write(parsed)
         config_file.close()
     except IOError :
         cli_parser.error( "Can't write apache configuration files")
-    
+
 try :
-        template_auth = open( options.configdir + 'svnauthz.template').read()
-        withrepo_auth = template_auth.replace('{-repository-}', options.repo)
-        witheverything_auth = withrepo_auth.replace('{-adminuser-}', options.adminuser )
-        
+    template_auth = Template( file( options.configdir + 'svnauthz.template').read() ))
+    withrepo_auth = template_auth.replace('{-repository-}', options.repo)
+    witheverything_auth = withrepo_auth.replace('{-adminuser-}', options.adminuser )
+    #### FIXME¡¡
+    auth_parsed = template_auth.safe_substitute(repository=options.repo ,
+    adminuser=username )
+
 except IOError :
-    cli_parser.error( "Repository auth template do not exist (RTFM)")
+    cli_parser.error( "Repository auth template do not exist")
 else :
     try:
-        config_file_auth = open(options.authdir + options.repo + '-svnauthz' ,'w')
-        config_file_auth.write(witheverything_auth)
+        config_file_auth = file(options.authdir + options.repo +
+        '-svnauthz','wb')
+        config_file_auth.write(auth_parsed)
         config_file_auth.close()
         lchown(options.authdir + options.repo + '-svnauthz',0,apachegid)
         chmod(options.authdir + options.repo + '-svnauthz',0640)
-        
+
     except IOError :
         cli_parser.error( "Can't write svnauthz files")
 
 try :
-	print "Please enter the desired password for: " + options.adminuser  
-	admin_user = call(['/usr/sbin/htpasswd2','-c',options.authdir + options.repo + '-passwdfile' ,options.adminuser])
-except OSError :
-        cli_parser.error( "Can't create password file")
+    username = raw_input("Please enter the desired repository admin username: ")
+    #password will not be printed to the stdout
+    password = getpass("Please enter the desired repository admin password: ")
+    ### Generate the string to enter into the htdigest file
+    kd = lambda x: md5.md5(':'.join(x)).hexdigest()
+    digest_data = ':'.join((username, options.repo , kd([username, options.repo , password])))
+    # write the data into the auth file used by apache
+    auth_file = file(options.authdir + options.repo + '-passwdfile','wb')
+    auth_file.write(digest_data)
+    auth_file.close()
 
-# value > 0 when errors found..
-if  not admin_user > 0:
-	lchown(options.authdir + options.repo + '-passwdfile',0,apachegid)
-	chmod(options.authdir + options.repo + '-passwdfile',0640)
-	
+except OSError :
+    cli_parser.error( "Can't create password file")
+
+lchown(options.authdir + options.repo + '-passwdfile',0,apachegid)
+chmod(options.authdir + options.repo + '-passwdfile',0640)
+
 try:
-		# how to select the filesystem type ? ( a case of WITFM)
-		#core.apr_initialize()
-		#pool = core.svn_pool_create(None)
-		#repos.svn_repos_create(options.location + options.repo, '', '', None, { fs.SVN_FS_CONFIG_FS_TYPE: .options.filesystem }, pool)
-		#core.svn_pool_destroy(pool)
-		#core.apr_terminate()
-		
-		# lets try the dirty way..
-		create = call(["svnadmin", "create", "--fs-type", options.filesystem, options.location + options.repo])
+    # we create the repository using the <rant> undocumented</rant> swig bindings.
+    # took me a while to figure how to do this.
+    # thanks to folks at #subversion-dev for give me some guidelines.
+    core.apr_initialize()
+    pool = core.svn_pool_create(None)
+    repos.svn_repos_create(options.location + options.repo, '', '', None, {
+        fs.SVN_FS_CONFIG_FS_TYPE: options.filesystem }, pool)
+    core.svn_pool_destroy(pool)
+    core.apr_terminate()
+
 except OSError:
-	cli_parser.error( "Failed to create the repository")
+    cli_parser.error( "Failed to create the repository")
 else:
-	for dire in ['dav','db','locks']:
-		lchown(options.location + options.repo + sep + dire,apacheuid,apachegid)
-		for root, dirs, files in walk(options.location + options.repo + sep + dire , topdown=False):
-			for name in files :
-				lchown(join(root, name),apacheuid,apachegid)
-			for name in dirs:
-				lchown(join(root, name),apacheuid,apachegid)
-				
-	print "Reloading apache.."   
-	restart_apache= call(["/etc/init.d/apache2", "reload"])
-	print "Basic repository setup complete ..Have a lot of Fun.. ;-)"
+    for dire in ['dav','db','locks']:
+        lchown(options.location + options.repo + sep + dire, apacheuid, apachegid)
+        for root, dirs, files in walk(options.location + options.repo + sep + dire ):
+            for name in files :
+                lchown(join(root, name) , apacheuid , apachegid)
+            for name in dirs:
+                lchown(join(root, name), apacheuid , apachegid)
+
+    print "Reloading apache.."
+     # there is no webserver.apache.reload() function yet :-)
+     # have to use a system call here..
+    call(["/etc/init.d/apache2", "reload"])
+    print "Basic repository setup complete ..Have a lot of Fun.. ;-)"
